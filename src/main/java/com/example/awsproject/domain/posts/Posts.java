f3104e74655c3c9f72521f0fba820cbf8eb1a83d
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-14 08:15:34 +0900

    feat: create posts Entity, Repository
    Posts
    PostsRepository

---
+++ src/main/java/com/example/awsproject/domain/posts/Posts.java
@@ -0,0 +1,25 @@
+package com.example.awsproject.domain.posts;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+}
