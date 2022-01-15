0c95cc084257f03eb7dd92f4f15df9b23e5e9783
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-15 10:51:13 +0900

    feat: implements mapping CRUD
    update
    findById
    delete

--- src/main/java/com/example/awsproject/web/PostsApiController.java
+++ src/main/java/com/example/awsproject/web/PostsApiController.java
@@ -1,11 +1,12 @@
 package com.example.awsproject.web;
 
+import com.example.awsproject.domain.posts.Posts;
 import com.example.awsproject.service.posts.PostsService;
+import com.example.awsproject.web.dto.PostsResponseDto;
 import com.example.awsproject.web.dto.PostsSaveRequestDto;
+import com.example.awsproject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +17,21 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/post/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
