433b13b39485ad0de5f2675ef01760eba50f5191
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-15 10:54:57 +0900

    feat: create indexController
    create indexController

---
+++ src/main/java/com/example/awsproject/web/indexController.java
@@ -0,0 +1,36 @@
+package com.example.awsproject.web;
+
+import com.example.awsproject.service.posts.PostsService;
+import com.example.awsproject.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class indexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
+    }
+}
