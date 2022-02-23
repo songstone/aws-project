00d15a5fab25b0632203b07d7299a26d2a1c4752
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-02-23 12:20:32 +0900

    config: security configuration

---
+++ src/main/java/com/example/awsproject/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.awsproject.config;
+
+import com.example.awsproject.config.auth.LoginArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginArgumentResolver loginArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginArgumentResolver);
+    }
+}
